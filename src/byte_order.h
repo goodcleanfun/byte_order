2815767dfc00cc41a6fcd5fb0c912a2037f61cb9
Al <albarrentine@gmail.com>
2025-05-14 03:01:45 -0400

    [float/double] adding float/double serialization using uint32_t and uint64_t

--- src/byte_order.h
+++ src/byte_order.h
@@ -24,11 +24,22 @@ static inline void write_uint16_little_endian(uint8_t* buffer, uint16_t value) {
     buffer[0] = (uint8_t)(value);
 }
 
+typedef union {
+    uint32_t u;
+    float f;
+} uint32_float_t;
+
 static inline uint32_t read_uint32_big_endian(const uint8_t* buffer) {
     return ((uint32_t)buffer[0] << 24) |
            ((uint32_t)buffer[1] << 16) |
            ((uint32_t)buffer[2] << 8) |
            ((uint32_t)buffer[3]);
+}
+
+static inline float read_float_big_endian(const uint8_t* buffer) {
+    uint32_float_t uf;
+    uf.u = read_uint32_big_endian(buffer);
+    return uf.f;
 }
 
 static inline void write_uint32_big_endian(uint8_t* buffer, uint32_t value) {
@@ -38,6 +49,12 @@ static inline void write_uint32_big_endian(uint8_t* buffer, uint32_t value) {
     buffer[3] = (uint8_t)(value);
 }
 
+static inline void write_float_big_endian(uint8_t* buffer, float value) {
+    uint32_float_t uf;
+    uf.f = value;
+    write_uint32_big_endian(buffer, uf.u);
+}
+
 static inline uint32_t read_uint32_little_endian(const uint8_t* buffer) {
     return ((uint32_t)buffer[3] << 24) |
            ((uint32_t)buffer[2] << 16) |
@@ -45,11 +62,23 @@ static inline uint32_t read_uint32_little_endian(const uint8_t* buffer) {
            ((uint32_t)buffer[0]);
 }
 
+static inline float read_float_little_endian(const uint8_t* buffer) {
+    uint32_float_t uf;
+    uf.u = read_uint32_little_endian(buffer);
+    return uf.f;
+}
+
 static inline void write_uint32_little_endian(uint8_t* buffer, uint32_t value) {
     buffer[3] = (uint8_t)(value >> 24);
     buffer[2] = (uint8_t)(value >> 16);
     buffer[1] = (uint8_t)(value >> 8);
     buffer[0] = (uint8_t)(value);
+}
+
+static inline void write_float_little_endian(uint8_t* buffer, float value) {
+    uint32_float_t uf;
+    uf.f = value;
+    write_uint32_little_endian(buffer, uf.u);
 }
 
 static inline uint64_t read_uint64_big_endian(const uint8_t* buffer) {
@@ -63,6 +92,17 @@ static inline uint64_t read_uint64_big_endian(const uint8_t* buffer) {
            ((uint64_t)buffer[7]);
 }
 
+typedef union {
+    uint64_t u;
+    double d;
+} uint64_double_t;
+
+static inline double read_double_big_endian(const uint8_t* buffer) {
+    uint64_double_t ud;
+    ud.u = read_uint64_big_endian(buffer);
+    return ud.d;
+}
+
 static inline void write_uint64_big_endian(uint8_t* buffer, uint64_t value) {
     buffer[0] = (uint8_t)(value >> 56);
     buffer[1] = (uint8_t)(value >> 48);
@@ -72,6 +112,12 @@ static inline void write_uint64_big_endian(uint8_t* buffer, uint64_t value) {
     buffer[5] = (uint8_t)(value >> 16);
     buffer[6] = (uint8_t)(value >> 8);
     buffer[7] = (uint8_t)(value);
+}
+
+static inline void write_double_big_endian(uint8_t* buffer, double value) {
+    uint64_double_t ud;
+    ud.d = value;
+    write_uint64_big_endian(buffer, ud.u);
 }
 
 static inline uint64_t read_uint64_little_endian(const uint8_t* buffer) {
@@ -85,6 +131,12 @@ static inline uint64_t read_uint64_little_endian(const uint8_t* buffer) {
            ((uint64_t)buffer[0]);
 }
 
+static inline double read_double_little_endian(const uint8_t* buffer) {
+    uint64_double_t ud;
+    ud.u = read_uint64_little_endian(buffer);
+    return ud.d;
+}
+
 static inline void write_uint64_little_endian(uint8_t* buffer, uint64_t value) {
     buffer[7] = (uint8_t)(value >> 56);
     buffer[6] = (uint8_t)(value >> 48);
@@ -94,6 +146,12 @@ static inline void write_uint64_little_endian(uint8_t* buffer, uint64_t value) {
     buffer[2] = (uint8_t)(value >> 16);
     buffer[1] = (uint8_t)(value >> 8);
     buffer[0] = (uint8_t)(value);
+}
+
+static inline void write_double_little_endian(uint8_t* buffer, double value) {
+    uint64_double_t ud;
+    ud.d = value;
+    write_uint64_little_endian(buffer, ud.u);
 }
 
 #endif /* BYTE_ORDER_H */
